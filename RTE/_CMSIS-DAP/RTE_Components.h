21f9dd005be07eda779199143136bc8d4d355bb4
Rhys Thomas <rhys97@me.com>
2017-11-13 10:52:37 +0000

    Initial commit.

---
+++ RTE/_CMSIS-DAP/RTE_Components.h
@@ -0,0 +1,23 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'web-server' 
+ * Target:  'CMSIS-DAP' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "MKW41Z4.h"
+
+#define RTE_RTOS_FreeRTOS_CONFIG        /* RTOS FreeRTOS Config for FreeRTOS API */
+#define RTE_RTOS_FreeRTOS_CORE          /* RTOS FreeRTOS Core */
+#define RTE_RTOS_FreeRTOS_HEAP_1        /* RTOS FreeRTOS Heap 1 */
+
+#endif /* RTE_COMPONENTS_H */
